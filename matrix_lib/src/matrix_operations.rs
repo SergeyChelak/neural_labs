939564e5cf4dbde8df49925b97884e1b9561e8d6
Sergey Chelak <sergey.chelak@gmail.com>
2022-12-11 00:19:05 +0200

    separated self-assign functions

--- matrix_lib/src/matrix_operations.rs
+++ matrix_lib/src/matrix_operations.rs
@@ -1,5 +1,4 @@
 use super::matrix::Matrix;
-use super::errors::*;
 
 impl Matrix {
     pub fn mul_scalar(&self, scalar: f64) -> Self {
@@ -16,27 +15,14 @@ impl Matrix {
     pub fn powi(&self, power: i32) -> Matrix {
         Self::map(self, |x| x.powi(power))
     }
-
-    pub fn plus_assign(&mut self, other: &Matrix) -> MathResult<()> {
-        self.modify_other(other, |a, b| a + b)        
-    }
-
-    pub fn minus_assign(&mut self, other: &Matrix) -> MathResult<()> {
-        self.modify_other(other, |a, b| a - b)
-    }
-
-    pub fn multiplicate_assign(&mut self, scalar: f64) {
-        self.modify(|x| x * scalar)
-    }
-
-    pub fn divide_assign(&mut self, scalar: f64) {
-        self.modify(|x| x / scalar)
-    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::{
+        *,
+        super::errors::*,
+    };
 
     #[test]
     fn matrix_operation_mul_scalar() -> MathResult<()> {
@@ -70,92 +56,6 @@ mod tests {
 
         ])?;
         assert!(a.transpose() == expected, "Matrix transpose implemented incorrectly");
-        Ok(())
-    }
-
-    #[test]
-    fn matrix_operation_add_assign() -> MathResult<()> {
-        let mut m1 = Matrix::from_vector(&vec![
-            vec![1.0, 2.0, 3.0],
-            vec![4.0, 5.0, 6.0],
-            vec![7.0, 8.0, 9.0]
-        ])?;
-
-        let m2 = Matrix::from_vector(&vec![
-            vec![9.0, 8.0, 7.0],
-            vec![6.0, 5.0, 4.0],
-            vec![3.0, 2.0, 1.0]
-        ])?;
-
-        let expected = Matrix::from_vector(&vec![
-            vec![10.0, 10.0, 10.0],
-            vec![10.0, 10.0, 10.0],
-            vec![10.0, 10.0, 10.0]
-        ])?;
-
-        m1.plus_assign(&m2)?;
-        assert!(m1 == expected, "Matrix add & assign implemented incorrectly");
-        Ok(())
-    }
-
-    #[test]
-    fn matrix_operation_sub_assign() -> MathResult<()> {
-        let mut m1 = Matrix::from_vector(&vec![
-            vec![1.0, 2.0, 3.0],
-            vec![4.0, 5.0, 6.0],
-            vec![7.0, 8.0, 9.0]
-        ])?;
-
-        let m2 = Matrix::from_vector(&vec![
-            vec![9.0, 8.0, 7.0],
-            vec![6.0, 5.0, 4.0],
-            vec![3.0, 2.0, 1.0]
-        ])?;
-
-        let expected = Matrix::from_vector(&vec![
-            vec![-8.0, -6.0, -4.0],
-            vec![-2.0,  0.0,  2.0],
-            vec![ 4.0,  6.0,  8.0]
-        ])?;
-
-        m1.minus_assign(&m2)?;
-        assert!(m1 == expected, "Matrix sub & assign implemented incorrectly");
-        Ok(())
-    }
-
-    #[test]
-    fn matrix_operation_scalar_multiplication() -> MathResult<()> {
-        let mut m = Matrix::from_vector(&vec![
-            vec![1.0, 2.0, 3.0],
-            vec![4.0, 5.0, 6.0],
-            vec![7.0, 8.0, 9.0]
-        ])?;
-
-        let expected = Matrix::from_vector(&vec![
-            vec![ 2.0,  4.0,  6.0],
-            vec![ 8.0, 10.0, 12.0],
-            vec![14.0, 16.0, 18.0]
-        ])?;
-        m.multiplicate_assign(2.0);
-        assert!(m == expected, "Matrix scalar multiplication & assign implemented incorrectly");
-        Ok(())
-    }
-
-    #[test]
-    fn matrix_operation_scalar_divide() -> MathResult<()> {
-        let expected = Matrix::from_vector(&vec![
-            vec![1.0, 2.0, 3.0],
-            vec![4.0, 5.0, 6.0],
-            vec![7.0, 8.0, 9.0]
-        ])?;
-
-        let mut m = Matrix::from_vector(&vec![
-            vec![ 2.0,  4.0,  6.0],
-            vec![ 8.0, 10.0, 12.0],
-            vec![14.0, 16.0, 18.0]
-        ])?;
-        m.divide_assign(2.0);
-        assert!(m == expected, "Matrix divide by scalar & assign implemented incorrectly");
         Ok(())
     }
 
