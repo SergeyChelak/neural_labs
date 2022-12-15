be9d101c93447fc55031955ce39d669a4b7cf72a
Sergey Chelak <sergey.chelak@gmail.com>
2022-12-15 19:33:24 +0200

    extended initializer

--- network_lib/src/convolution_layer.rs
+++ network_lib/src/convolution_layer.rs
@@ -1,16 +1,27 @@
+use std::vec;
+
 use super::layer::*;
 use matrix_lib::{
+    dimensions::Dimensions, 
     errors::*,
     matrix::Matrix,
 };
 
 pub struct Convolution {
-    //
+    input_depth: usize,
+    kernels: Vec<Matrix>,
+    biases: Vec<Matrix>,
+    input: Matrix,
 }
 
 impl Convolution {
-    pub fn new(input_shape: usize, kernel_size: usize, depth: usize) -> Self {
-        Self {}
+    pub fn new(input_dimensions: Dimensions, input_depth: usize, kernel_dimension: usize, kernel_depth: usize) -> Self {
+        Self {
+            input_depth,
+            kernels: vec![],
+            biases: vec![],
+            input: Matrix::empty(),
+        }
     }
 }
 
