1085769a843f177834954dfd772c51ed404a32ab
Sergey Chelak <sergey.chelak@gmail.com>
2022-12-26 21:27:24 +0200

    full convolution

--- matrix_lib/src/convolution.rs
+++ matrix_lib/src/convolution.rs
@@ -30,3 +30,65 @@ pub fn cross_correlation(input: &Matrix, kernel: &Matrix) -> MathResult<Matrix> 
     }
     Ok(output)
 }
+
+fn padding_expand(input: &Matrix, kernel_size: &Dimensions, fill_value: f64) -> MathResult<Matrix> {
+    let (input_rows, input_cols) = input.dimensions().as_tuple();
+    let (kernel_rows, kernel_cols) = kernel_size.as_tuple();
+    if kernel_cols == 0 || kernel_rows == 0 {
+        return Err(MathError::IncorrectMatricesDimensions(
+            "Padding matrix expand".to_string(), input.dimensions(), kernel_size.to_owned())
+        );
+    }
+    let row_offset = kernel_rows - 1;
+    let col_offset = kernel_cols - 1;
+    let row_range = row_offset..=input_rows;
+    let col_range = col_offset..=input_cols;
+    let matrix = Matrix::new(
+        input_rows + 2 * row_offset, 
+        input_cols + 2 * col_offset, 
+        |i, j| {
+            if row_range.contains(&i) && col_range.contains(&j) {
+                input[i - row_offset][j - col_offset]
+            } else {
+                fill_value
+            }
+        }
+    );
+    Ok(matrix)
+}
+
+pub fn full_convolution(input: &Matrix, kernel: &Matrix) -> MathResult<Matrix> {
+    let expanded = padding_expand(input, &kernel.dimensions(), 0.0)?;
+    cross_correlation(&expanded, kernel)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn padding_expand_test() -> MathResult<()> {
+        let input = Matrix::from_vector(&vec![
+            vec![1.0, 6.0, 2.0],
+            vec![5.0, 3.0, 1.0],
+            vec![7.0, 0.0, 4.0],
+        ])?;
+    
+        let kernel = Matrix::from_vector(&vec![
+            vec![ 1.0, 2.0],
+            vec![-1.0, 0.0],
+        ])?;
+
+        let expected = Matrix::from_vector(&vec![
+            vec![0.0, 0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 1.0, 6.0, 2.0, 0.0],
+            vec![0.0, 5.0, 3.0, 1.0, 0.0],
+            vec![0.0, 7.0, 0.0, 4.0, 0.0],
+            vec![0.0, 0.0, 0.0, 0.0, 0.0],
+        ])?;
+        let result = padding_expand(&input, &kernel.dimensions(), 0.0)?;
+        println!("{:?}", result);
+        assert!(expected == result, "Padding expand works incorrectly");
+        Ok(())
+    }
+}
--- matrix_lib/tests/convolution_test.rs
+++ matrix_lib/tests/convolution_test.rs
@@ -54,3 +54,28 @@ fn convolution_cross_correlation2() -> MathResult<()> {
     assert!(expected == result, "Cross-corrleation calculated incorrectly");
     Ok(())
 }
+
+#[test]
+fn convolution_full_conv() -> MathResult<()> {
+    let input = Matrix::from_vector(&vec![
+        vec![1.0, 6.0, 2.0],
+        vec![5.0, 3.0, 1.0],
+        vec![7.0, 0.0, 4.0],
+    ])?;
+
+    let kernel = Matrix::from_vector(&vec![
+        vec![ 1.0, 2.0],
+        vec![-1.0, 0.0],
+    ])?;
+
+    let expected = Matrix::from_vector(&vec![
+        vec![ 0.0, -1.0, -6.0, -2.0],
+        vec![ 2.0,  8.0,  7.0,  1.0],
+        vec![10.0,  4.0,  5.0, -3.0],
+        vec![14.0,  7.0,  8.0,  4.0],
+    ])?;
+
+    let result = full_convolution(&input, &kernel)?;
+    assert!(expected == result, "Full convolution calculated incorrectly");
+    Ok(())
+}
