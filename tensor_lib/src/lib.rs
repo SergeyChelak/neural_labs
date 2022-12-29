28d150f44d55f0570dc5fa7d43909c236a9e7377
Sergey Chelak <sergey.chelak@gmail.com>
2022-12-29 17:25:30 +0200

    stub for tensor construct

--- tensor_lib/src/lib.rs
+++ tensor_lib/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod common;
 pub mod tensor_shape;
 pub mod tensor;
+pub mod tensor_construct;
---
+++ tensor_lib/src/tensor_construct.rs
@@ -0,0 +1,11 @@
+use super::tensor::Tensor;
+
+impl<T: Copy> Tensor<T> {
+    pub fn vector(arr: Vec<T>) -> Self {
+        todo!()
+    }
+
+    pub fn matrix(arr: Vec<Vec<T>>) -> Self {
+        todo!()
+    }
+}
