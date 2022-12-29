6a480afff5169ff0945ae708d2486565c803829e
Sergey Chelak <sergey.chelak@gmail.com>
2022-12-29 15:47:13 +0200

    implemented basic methods

--- tensor_lib/src/tensor.rs
+++ tensor_lib/src/tensor.rs
@@ -1,5 +1,3 @@
-use std::ops::Deref;
-
 use super::tensor_error::*;
 
 pub type TensorIndex = Vec<usize>;
@@ -8,11 +6,28 @@ pub type TensorShape = Vec<usize>;
 pub struct Tensor<T> {
     buffer: Vec<T>,
     shape: TensorShape,
+    shape_offsets: TensorShape,
 }
 
 impl<T: Copy> Tensor<T> {
-    pub fn new() -> Self {
+    pub fn new(shape: TensorShape) -> Self {
+        let shape_offs = Self::shape_offsets(&shape);
         todo!("create new tesor filled with zeros")
+    }
+
+    fn shape_offsets(shape: &TensorShape) -> TensorShape {
+        let size = shape.len();
+        if size == 0 {
+            return vec![];
+        }
+        let mut result: TensorShape = Vec::with_capacity(size);
+        result.push(1);
+        for i in 0..size-1 {
+            let rate = result[i] * shape[size - i - 1];
+            result.push(rate);
+        }
+        result.reverse();
+        result
     }
         
     pub fn element_wise<F>(&mut self, func: F) where F: Fn(T) -> T {
@@ -34,7 +49,8 @@ impl<T: Copy> Tensor<T> {
     }
 
     pub fn get_unchecked(&self, index: &TensorIndex) -> T {
-        todo!("get unchecked tensor element")
+        let buf_idx = self.buffer_index(index);
+        self.buffer[buf_idx]
     }
 
     pub fn set(&mut self, index: &TensorIndex, value: T) -> TensorResult<()> {
@@ -46,24 +62,45 @@ impl<T: Copy> Tensor<T> {
     }
 
     pub fn set_unchecked(&mut self, index: &TensorIndex, value: T) {
-        todo!("set unchecked tensor element")
+        let buf_idx = self.buffer_index(index);
+        self.buffer[buf_idx] = value;
     }
 
     fn buffer_index(&self, index: &TensorIndex) -> usize {
-        todo!("internal buffer index")
+        self.shape_offsets.iter()
+            .zip(index.iter())
+            .map(|(offs, idx)| offs * idx)
+            .fold(0usize, |acc, v| acc + v)
     }
 
     pub fn is_same_shape(&self, other: &Tensor<T>) -> bool {
-        todo!("Validate shape")
+        if !self.is_same_rank(other) {
+            return false;
+        }
+        self.shape.iter()
+            .zip(other.shape.iter())
+            .map(|(a, b)| a == b)
+            .fold(true, |acc, v| acc && v)
     }
 
     pub fn is_valid_index(&self, index: &TensorIndex) -> bool {
-        todo!("Validate index")
+        if index.len() != self.shape.len() {
+            return false;
+        }
+        self.shape.iter()
+            .zip(index.iter())
+            .map(|(shape, index)| shape > index )
+            .fold(true, |acc, v| acc && v)
     }
 
     #[inline(always)]
     pub fn rank(&self) -> usize {
         self.shape.len()
+    }
+
+    #[inline(always)]
+    pub fn is_same_rank(&self, other: &Tensor<T>) -> bool {
+        self.rank() != other.rank()
     }
 }
 
